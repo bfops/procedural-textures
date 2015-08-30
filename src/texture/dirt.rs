4b7176d492867fc6808c38b66d19e731cd934e2d
Zeke Foppa <benjamin.foppa@gmail.com>
2015-08-30 14:54:52 -0400

    sharper dirt texture

--- src/texture/dirt.rs
+++ src/texture/dirt.rs
@@ -15,26 +15,30 @@ impl ::texture::T for T {
   fn eval(&self, x: f32, y: f32, z: f32) -> [f32; 3] {
     let seed = noise::Seed::new(0);
 
-    let perlin = |f, x, y, z| {
-      noise::perlin3(&seed, &[f*x, f*y, f*z])
-    };
-
     let waves = [
       Wave { freq: 8.0,   amp: 1.0 },
-      Wave { freq: 16.0,  amp: 0.5 },
-      Wave { freq: 128.0, amp: 0.2 },
+      Wave { freq: 16.0,  amp: 0.6 },
+      Wave { freq: 64.0,  amp: 0.2 },
+      Wave { freq: 128.0, amp: 0.4 },
     ];
 
     let mut d = 0.0;
     let mut amp = 0.0;
     for wave in waves.iter() {
-      d = d + perlin(wave.freq, x, y, z) * wave.amp;
+      let dd = noise::perlin3(&seed, &[wave.freq*x, wave.freq*y, wave.freq*z]);
+      // sharpen
+      let dd = dd.signum() * dd.abs().sqrt();
+      d = d + dd * wave.amp;
       amp = amp + wave.amp;
     }
 
-    // Map to [0,1]
-    let d = (d + amp) / (2.0 * amp);
+    // map to [-1, 1]
+    let d = d / amp;
+    // map to [0,1]
+    let d = (d + 1.0) / 2.0;
 
-    [0.4 + d*0.3, 0.3 + d*0.3, 0.1 + d*0.3]
+    let lerp = 0.3 * d;
+
+    [0.4 + lerp, 0.3 + lerp, 0.1 + lerp]
   }
 }
